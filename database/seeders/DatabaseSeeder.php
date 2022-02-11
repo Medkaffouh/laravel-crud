f1879052ad86c035005c10d49a19e94a90f19079
Medkaffouh <kaffouhmohamed@gmail.com>
2022-02-11 21:16:43 +0100

    add factory fonctionality to generate data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(ProductSeeder::class);
+         \App\Models\User::factory(10)->create();
+         \app\Models\Product::factory(50)->create();
+        //$this->call(ProductSeeder::class);
     }
 }
